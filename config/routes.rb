60224c7234e11341a0f90527f10c8140b00ab158
Jason Wharff <fishermanswharff@mac.com>
2016-05-01 06:49:10 -0400

    routes, application config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root 'home#index'
 
   match '/users/auth/:provider' => 'users/omniauth_callbacks#passthru', via: [:get,:post]
   match '/users/auth/twitter/callback' => 'users/omniauth_callbacks#twitter', via: [:get,:post]
