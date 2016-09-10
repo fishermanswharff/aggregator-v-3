c2fef32ffffda29fa24f9f75549488719d77c804
Jason Wharff <fishermanswharff@mac.com>
2016-09-09 22:02:29 -0400

    api_only = false

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,7 @@ module Gator3
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
     config.encoding = 'utf-8'
-    config.filter_parameters += [:password, :password_confirmation, :new_password, :new_password_confirmation]
-    # config.api_only = false
+    config.api_only = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
