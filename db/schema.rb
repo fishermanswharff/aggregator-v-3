30b1d97c82620401110a2396d7625e9bad6b06a6
Jason Wharff <fishermanswharff@mac.com>
2016-09-13 00:16:54 -0400

    migration/model for session in active record store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506013719) do
+ActiveRecord::Schema.define(version: 20160913032211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,16 @@ ActiveRecord::Schema.define(version: 20160506013719) do
 
   add_index "followers", ["followable_type", "followable_id"], name: "index_followers_on_followable_type_and_followable_id", using: :btree
   add_index "followers", ["user_id", "followable_id", "followable_type"], name: "index_followers_on_user_followable_id_followable_type", unique: true, using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "topics", force: :cascade do |t|
     t.string   "name",       default: "", null: false
