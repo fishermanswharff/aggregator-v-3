d4c2fc6c00768526ab8e033a155f6839f6095d03
Jason Wharff <fishermanswharff@mac.com>
2016-09-13 00:17:47 -0400

    commenting out some unused variables, skip_before_action on sessions_controller

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'uri'
+require 'open-uri'
 
 class Crawler
   def crawl(url)
-    page = Nokogiri::HTML(open(url))
-    page_uri = URI(url)
-    robots_txt = `curl #{url.gsub(/\/$/, '')}/robots.txt`
+    page = Nokogiri::HTML(open(url).read)
+    # page_uri = URI(url)
+    # robots_txt = `curl #{url.gsub(/\/$/, '')}/robots.txt`
 
     link_attributes = page.xpath('//head').flat_map(&:children).select { |child| child.name == 'link' }.map(&:attributes)
     rss_links = link_attributes.select { |link| link['type']&.value == 'application/rss+xml' }
@@ -20,6 +21,7 @@ class Crawler
         end
       end
     rescue ActiveRecord::RecordNotUnique => e
+      Rails.logger.debug "————————— ActiveRecord::RecordNotUnique #{e}—————————"
       # do something?
     end
   end
