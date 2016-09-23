8d3183bcf39a70da27dac574b55b5463da076cb6
Jason Wharff <fishermanswharff@mac.com>
2016-09-23 18:41:52 -0400

    filter oauth params from logger

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :password_confirmation, :new_password, :new_password_confirmation]
+Rails.application.config.filter_parameters += [
+  :password,
+  :password_confirmation,
+  :new_password,
+  :new_password_confirmation,
+  :provider,
+  :oauth_token,
+  :oauth_verifier,
+  'user-token'
+]
